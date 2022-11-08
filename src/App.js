7200c74a41b992298b4ef0a2b109bde54fedfd93
Shama <assiddik001@gmail.com>
2022-11-09 01:53:34 +0600

    add sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
 import Topbar from "./sections/Topbar";
 import { ColorModeContext, useMode } from "./theme";
+import Dashboard from "./pages/dashboard";
+import Sidebar from "./sections/Sidebar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -10,8 +13,12 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar />
           <main className="content">
             <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
