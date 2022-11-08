0fd363e401e46612016891de2e7899d73e318d8f
Shama <assiddik001@gmail.com>
2022-11-08 23:35:57 +0600

    add topbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./sections/Topbar";
 import { ColorModeContext, useMode } from "./theme";
 
 function App() {
@@ -9,7 +10,9 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <main className="content"></main>
+          <main className="content">
+            <Topbar />
+          </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
