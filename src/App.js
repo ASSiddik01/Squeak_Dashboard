09089369d89226b662876baa46b4780bb6bff34b
Shama <assiddik001@gmail.com>
2022-11-09 12:09:50 +0600

    user from

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Sidebar from "./sections/Sidebar";
 import Team from "./pages/team";
 import Contacts from "./pages/contacts";
 import Invoices from "./pages/invoices";
+import Form from "./pages/form";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -24,6 +25,7 @@ function App() {
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
             </Routes>
           </main>
         </div>
