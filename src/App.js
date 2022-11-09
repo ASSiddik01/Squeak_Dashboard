a8c1771e9a0221672e18ccdc97c6e7deeee2853b
Shama <assiddik001@gmail.com>
2022-11-09 11:24:45 +0600

    add manage team

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Topbar from "./sections/Topbar";
 import { ColorModeContext, useMode } from "./theme";
 import Dashboard from "./pages/dashboard";
 import Sidebar from "./sections/Sidebar";
+import Team from "./pages/team";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -18,6 +19,7 @@ function App() {
             <Topbar />
             <Routes>
               <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
             </Routes>
           </main>
         </div>
